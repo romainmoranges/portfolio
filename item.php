308d822eb513e0e8643e420e0218b6aa82e87ed3
Romain Moranges <romain.moranges@gmail.com>
2019-02-22 20:25:20 +0100

    on est pas mal !

--- item.php
+++ item.php
@@ -1,11 +1,28 @@
 <?php include 'header.php'; 
-$reqItem = $bdd->prepare('SELECT `titre`, `resume`, `image`, `libelle_cat` FROM items i INNER JOIN categories c ON i.id_cat = c.id_cat WHERE i.id_item= :id_item');
+$reqItem = $bdd->prepare('SELECT `titre`, `resume`, `image`, `libelle_cat`, `url` FROM items i INNER JOIN categories c ON i.id_cat = c.id_cat WHERE i.id_item= :id_item');
 $reqItem->bindValue(':id_item', $_GET['id_item']);
 $reqItem->execute();
 
 $item = $reqItem->fetchAll(PDO::FETCH_OBJ);
+
+$input = $item[0]->libelle_cat;
+switch ($input) {
+    case 'Développement':
+        $class = 'developpement';
+        break;
+    case 'Intégration':
+        $class = 'integration';
+        break;
+    case 'Infographie':
+        $class = "infographie";
+        break;
+    case 'Communication':
+        $class = 'communication';
+        break;
+}
+
 ?>
-<section id="projet">
+<section id="projet" class="<?= $class ?>">
     <div class="inner">
         <a href="/#portfolio">
             < Retour</a> 
@@ -19,13 +36,17 @@ $item = $reqItem->fetchAll(PDO::FETCH_OBJ);
 
                 <?php 
 if(!empty($item[0]->image)){
-    echo '<div class="project-img"><img src="./img/portfolio/' . $item[0]->image . '"/></div>';
+    echo '<div class="project-img"><img src="' . $item[0]->image . '"/></div>';
 }
 
 if ($item[0]->libelle_cat == 'Développement' || $item[0]->libelle_cat == 'Intégration' ) {
-    echo '<p>Vous pouvez visiter <b><a href="https://github.com/romainmoranges">ma page Github                    <svg class="svgicon">
+    echo '<p>Vous pouvez visiter <b><a href="' . $item[0]->url . '">ma page Github                    <svg class="svgicon">
     <use xlink:href="#github"></use>
-</svg></a></b> pour retrouver les sources de mes projets !</p> ';
+</svg></a></b> pour retrouver les sources de ce projet !</p> ';
+} else {
+    if (!empty($item[0]->url)) {
+    echo '<p>Vous pouvez consulter ce document <b><a target="_blank" rel="noopener noreferrer" href="' . $item[0]->url . '"> à cette adresse</a></b>';
+    }
 }
 ?>
     </div>
